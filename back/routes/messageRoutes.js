15338bfd3fb893a40d4f9d837aa3511dc30896d0
malu-sree <sreelakshmipalangattu@gmail.com>
2024-12-05 12:38:03 +0530

    c-5

--- back/routes/messageRoutes.js
+++ back/routes/messageRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { sendMessage } = require("../controllers/messageController");
+const { sendMessage,getMessages } = require("../controllers/messageController");
 const protectRoute = require("../middleware/protectRoute");
 
 router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages);
+ 
 
 module.exports = router;
